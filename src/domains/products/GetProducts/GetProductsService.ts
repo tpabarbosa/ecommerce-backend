055ef080a93c8a2589b4a4b6bbd7234cc58abdfd
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-08-12 08:55:39 -0300

    fix search query may not be present

--- src/domains/products/GetProducts/GetProductsService.ts
+++ src/domains/products/GetProducts/GetProductsService.ts
@@ -38,6 +38,12 @@ export default class GetProductsService extends BaseService<
 
     const order = PaginationHelper.getOrder(data.orderBy, 'products');
 
+    let search: string[];
+    if (data.search) {
+      const searchArr = data.search.split(' ');
+      search = searchArr.map((word) => `%${word}%`);
+    }
+
     const products = await this.repository.product
       .createQueryBuilder('products')
       .leftJoinAndSelect('products.categories', 'categories')
@@ -46,9 +52,9 @@ export default class GetProductsService extends BaseService<
       .leftJoinAndSelect('products.photos', 'photos')
       .where(
         data.search
-          ? `products.name ILIKE :search OR products.description ILIKE :search OR photos.description ILIKE :search OR (sale.campaing ILIKE :search AND sale.start_date <= NOW() AND sale.end_date >= NOW()) OR categories.name ILIKE :search OR sizes.name ILIKE :search`
+          ? `products.name ILIKE ANY(:search) OR products.description ILIKE ANY(:search) OR photos.description ILIKE ANY(:search) OR (sale.campaing ILIKE ANY(:search) AND sale.start_date <= NOW() AND sale.end_date >= NOW()) OR categories.name ILIKE ANY(:search) OR sizes.name ILIKE ANY(:search)`
           : '1=1',
-        { search: `%${data.search}%` }
+        { search }
       )
       .andWhere(where && !data.search ? where : '1=1', queriesValues)
       .orWhere(where && data.search ? where : '1=0', queriesValues)
@@ -66,9 +72,9 @@ export default class GetProductsService extends BaseService<
       .leftJoinAndSelect('products.photos', 'photos')
       .where(
         data.search
-          ? `products.name ILIKE :search OR products.description ILIKE :search OR photos.description ILIKE :search OR (sale.campaing ILIKE :search AND sale.start_date <= NOW() AND sale.end_date >= NOW()) OR categories.name ILIKE :search OR sizes.name ILIKE :search`
+          ? `products.name ILIKE ANY(:search) OR products.description ILIKE ANY(:search) OR photos.description ILIKE ANY(:search) OR (sale.campaing ILIKE ANY(:search) AND sale.start_date <= NOW() AND sale.end_date >= NOW()) OR categories.name ILIKE ANY(:search) OR sizes.name ILIKE ANY(:search)`
           : '1=1',
-        { search: `%${data.search}%` }
+        { search }
       )
       .andWhere(where && !data.search ? where : '1=1', queriesValues)
       .orWhere(where && data.search ? where : '1=0', queriesValues)
