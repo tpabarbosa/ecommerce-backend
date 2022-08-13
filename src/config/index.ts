0d0c76d892eff3932a190e6a79249c75fb1cd7e8
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-08-13 14:55:23 -0300

    add vercel config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const config = {
-  port: process.env.APP_PORT || 5000,
+  port: process.env.APP_PORT || process.env.PORT || 5000,
   host: process.env.APP_HOST || 'http://localhost',
   jwt_private_key: process.env.JWT_PRIVATE_KEY,
 };
